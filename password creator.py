8c5bfcecf6ca4719d84006f9a4110420a2c6d793
Pythonuser72929 <115384842+Pythonuser72929@users.noreply.github.com>
2022-10-08 23:14:15 -0500

    Update password creator.py

--- password creator.py
+++ password creator.py
@@ -1,12 +1,12 @@
 import random as rd
-def contraseña():
+def password():
    lettersmax = "QWERTYUIOPASDFGHJKLÑZXCVBNM"
    lettersmin = "qwertyuiopasdfghjklñzxcvbnm"
    numbers = "123456789"
    print(rd.choice(lettersmax),rd.choice(lettersmax),rd.choice(lettersmin),rd.choice(numbers),rd.choice(numbers),rd.choice(numbers))
-   loop = str(input("cargar otra contraseña?  no/si: "))
-   if loop == "si" or loop == "yes":
-   	contraseña()
+   loop = str(input("load another password?  no/yes: "))
+   if loop == "yes":
+   	password()
    else:
-   	print("fin del programa")
-contraseña()   		
+   	print("end")
+password()   		
