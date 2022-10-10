ec2cd61563820db53299e519de8b4b167cf462db
Pythoncode <115384842+Pythonuser72929@users.noreply.github.com>
2022-10-09 22:18:23 -0500

    Update password creator.py

--- password creator.py
+++ password creator.py
@@ -4,9 +4,9 @@ def password():
    lettersmin = "qwertyuiopasdfghjklñzxcvbnm"
    numbers = "123456789"
    print(rd.choice(lettersmax),rd.choice(lettersmax),rd.choice(lettersmin),rd.choice(numbers),rd.choice(numbers),rd.choice(numbers))
-   loop = str(input("load another password?  no/yes: "))
-   if loop == "yes":
+   loop = str(input("press enter (load another password): "))
+   if loop == "":
    	password()
    else:
-   	print("end")
-password()   		
+   	print("invalid comand only press enter(reestart and try again)")   	
+password()   
